da8a6f14890277eae4218991507526a913a47cc2
DINO7602 <Phongnbde160087@fpt.edu.vn>
2024-03-05 00:14:47 +0700

    Cau hinh Router vs xay dung co che Layout

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/page/Home';
+import Following from '~/page/Following';
+import Profile from '~/page/Profile';
+import Upload from '~/page/Upload';
+import Search from '~/page/Search';
+
+const publicRoutes = [
+    { path: '', component: Home },
+    { path: '/following', component: Following },
+    { path: '/Profile', component: Profile },
+    { path: '/Upload', component: Upload, layout: HeaderOnly },
+    { path: '/Search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
